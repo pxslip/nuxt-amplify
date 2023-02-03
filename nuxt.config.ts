cf79cc0dbdd3ab98c2e45523d807d00b8b6d1ba6
Will K (pxslip) <dev@pxslip.com>
2023-02-03 20:26:36 +0000

    Trick amplify?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
+import { join, resolve } from 'path';
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const outputDir = resolve('./.output');
+const standaloneDir = join(outputDir, 'standalone');
 export default defineNuxtConfig({
   nitro: {
     preset: undefined,
+    output: {
+      dir: outputDir,
+      serverDir: standaloneDir,
+      publicDir: standaloneDir,
+    },
   },
   vite: {
     define: {
